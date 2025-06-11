ce9759aebc9b2490dc9db508e81038878540b533
Vaibhav Seth <sethvaibhav95@gmail.com>
2025-06-11 12:45:35 +0200

    chore: remove overrides and add macos ci

---
+++ commitlint.parser-config.js
@@ -0,0 +1,1 @@
+module.exports = { parserOpts: { headerPattern: /^([^:]+):\s*(.*)$/ } };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-const path = require('path');
-const { run, flush, handle } = require('@oclif/core');
-const pjson = require('../oclif.config.json');
+const { run } = require('@oclif/core');
 
-run(undefined, {
-  root: path.join(__dirname, '..'),
-  pjson,
-})
-  .then(flush)
-  .catch(handle);
+run().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
